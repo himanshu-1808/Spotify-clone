1249d0d358c7e14785986cd651497028a7055e9c
Himanshu <75906597+himanshu-1808@users.noreply.github.com>
2021-05-06 20:38:57 +0530

    Third Commit

--- spotify-clone/src/Footer.js
+++ spotify-clone/src/Footer.js
@@ -5,28 +5,46 @@ import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 import RepeatIcon from '@material-ui/icons/Repeat';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import { Grid, Slider } from "@material-ui/core";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay"
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
 
 
-function Footer(){
-    return(
+
+function Footer() {
+    return (
         <div className="footer">
-<div className="footer_left">
-<p>Album</p>
-</div>
-<div className="footer_center">
-<ShuffleIcon className="footer_green"/>
-<SkipPreviousIcon className="footer_icon"/>
-<PlayCircleOutlineIcon fontSize="large" className="footer_icon"/>
-<SkipNextIcon className="footer_icon"/>
-<RepeatIcon className="footer_green"/>
-
-
-
-</div>
-<div className="footer_right">
-<p>volume</p>
-</div>
+            <div className="footer_left">
+                <img className="albumLogo" src="https://upload.wikimedia.org/wikipedia/en/f/f7/XXXTentacion_%E2%80%93_Skins.png" alt="" />
+                <div className="footer_songInfo">
+                    <h4>No Songs</h4>
+                    <p>....</p>
+                </div>
             </div>
+            <div className="footer_center">
+                <ShuffleIcon className="footer_green" />
+                <SkipPreviousIcon className="footer_icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+                <SkipNextIcon className="footer_icon" />
+                <RepeatIcon className="footer_green" />
+
+
+
+            </div>
+            <div className="footer_right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
     )
 }
 export default Footer;
